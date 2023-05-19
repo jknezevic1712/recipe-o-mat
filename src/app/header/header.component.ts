c03213f5e9c97a1b3628af70d59835d4497a3a0e
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-05-19 21:32:44 +0200

    Header component done for now.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  host: {
+    '(document:click)': 'menuOutsideClickHandler($event)',
+  },
 })
 export class HeaderComponent {
   isMenuOpen = false;
-  user = null;
+  user: any = null;
+
+  @ViewChild('navButtonElement', { static: false })
+  navButtonElementRef!: ElementRef;
+
+  constructor(private _elRef: ElementRef) {}
+
+  handleAuth() {
+    if (!this.user) {
+      this.user = 'User loaded!';
+      console.log('Signed in!');
+    } else {
+      this.user = null;
+      console.log('Signed out!');
+    }
+  }
 
   handleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  handleSignOut() {
-    console.log('Signed out!');
+  handleKeyboardMenuClose(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      return (this.isMenuOpen = false);
+    }
+
+    return true;
+  }
+
+  menuOutsideClickHandler(e: MouseEvent) {
+    if (
+      this.isMenuOpen === true &&
+      e.target !== null &&
+      !this._elRef.nativeElement.children[0].children[1].contains(
+        e.target as Node
+      )
+    ) {
+      this.isMenuOpen = false;
+    }
   }
 }
