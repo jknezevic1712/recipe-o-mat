3c4c61d740e04f9540b542ac0345f9dd5684778f
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-05-30 22:05:24 +0200

    Reverted using button component due to bugs with dynamic children text. Added signUpForm.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,7 +8,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AuthComponent implements OnInit, OnDestroy {
   isSignUpMode = false;
-  authForm: FormGroup<{
+  signInForm: FormGroup<{
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }>;
+  signUpForm: FormGroup<{
+    firstName: FormControl<string>;
+    lastName: FormControl<string>;
     email: FormControl<string>;
     password: FormControl<string>;
   }>;
@@ -22,12 +28,15 @@ export class AuthComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {}
 
   private initForm() {
+    let firstName = '';
+    let lastName = '';
     let email = '';
+    let password = '';
 
     if (this.isSignUpMode) {
-      let password = '';
-
-      this.authForm = new FormGroup({
+      this.signUpForm = new FormGroup({
+        firstName: new FormControl(firstName, Validators.required),
+        lastName: new FormControl(lastName, Validators.required),
         email: new FormControl(email, [Validators.email, Validators.required]),
         password: new FormControl(password, [
           Validators.required,
@@ -35,9 +44,7 @@ export class AuthComponent implements OnInit, OnDestroy {
         ]),
       });
     } else {
-      let password = '';
-
-      this.authForm = new FormGroup({
+      this.signInForm = new FormGroup({
         email: new FormControl(email, [Validators.email, Validators.required]),
         password: new FormControl(password, [
           Validators.required,
@@ -53,5 +60,6 @@ export class AuthComponent implements OnInit, OnDestroy {
 
   onSignUpHandler() {
     this.isSignUpMode = !this.isSignUpMode;
+    this.initForm();
   }
 }
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -8,6 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() clickHandlerProp: () => void;
   @Input() disabledProp: boolean;
+  @Input() btnTypeProp: 'button' | 'submit' | 'reset';
+  @Input() btnTextProp: string;
   @Input() ngClassesBtnProp: string;
   @Input() ngClassesDivProp: string;
   @Input() ngClassesSpanProp: string;
