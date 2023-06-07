1e74b2a76b6381a7aa377f6bbac58acbf9ab7f01
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-06-07 13:52:21 +0200

    Added loading spinner component.

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
 import { ShareIconsModule } from 'ngx-sharebuttons/icons';
 
@@ -12,7 +13,7 @@ import { RecipesListComponent } from './recipe-list/recipes-list.component';
 import { RecipeTileComponent } from './recipe-tile/recipe-tile.component';
 import { RecipeStoreComponent } from './recipe-store/recipe-store.component';
 import { RecipeCommentComponent } from './recipe-detail/recipe-comment/recipe-comment.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingSpinnerComponent } from '../shared/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     RecipesListComponent,
     RecipeStoreComponent,
     RecipeCommentComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [
     CommonModule,
